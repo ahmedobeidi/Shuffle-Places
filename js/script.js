27aef754cfabd5f972d149e3df102ee417f2b98e
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-02 21:09:32 +0100

    ADD: Delete button

--- js/script.js
+++ js/script.js
@@ -1,83 +1,122 @@
 
-let stuendts = [];
-let theList = [];
+// Lists
+let names = [];
+let originalList = [];
 let randomList = [];
+let itemID = 0;
 
-const inputText = document.querySelector("#textinput");
+const inputText = document.getElementById("textinput");
 const participantsList1 = document.getElementById("participants-list-1");
 const participantsList2 = document.getElementById("participants-list-2");
 const participantsList3 = document.getElementById("participants-list-3");
 
-
+const addButton = document.querySelector("#addButton");
 const randomButton = document.getElementById("randomButton");
+
+addButton.addEventListener("click",handleAddButton) ;
 randomButton.addEventListener("click", handleRandomButton);
 
-const addButton = document.querySelector("#addButton")
-addButton.addEventListener("click",handleOnClickAdd) ;
-function handleOnClickAdd() {
-    
+function handleAddButton() {
+
     let value = inputText.value;
     value = value.toLowerCase();
     value = value.charAt(0).toUpperCase() + value.slice(1);
     
-    for(let i = 0; i < theList.length; i++) {
-        stuendts[i] = theList[i];
+    for(let i = 0; i < originalList.length; i++) {
+        names[i] = originalList[i];
     }
     
     if (value == "") return;
     if (value.length > 9) return;
 
-    if (stuendts.length < 5) {
-        addToList(participantsList1, value);
-        inputText.value = "";
+    if (names.length < 5) {
+      addToTheList(participantsList1, value);
     }
-    else if (stuendts.length < 10) {
-        
-        addToList(participantsList2, value);
-        inputText.value = "";
+    else if (names.length < 10) {
+      addToTheList(participantsList2, value);
     }
-    else if (stuendts.length < 15) {
-        addToList(participantsList3, value);
-        inputText.value = "";
+    else if (names.length < 15) {
+      addToTheList(participantsList3, value);
     }
-
 }
 
-function addToList(list, value) {
+function addToTheList(participantsList, value) {
 
-    list.classList.remove("hidden");
+  participantsList.classList.remove("hidden");
 
-    const listItem = document.createElement("li");
-    const textSpan = document.createElement("p");
+  const listItem = document.createElement("li");
+  const textSpan = document.createElement("span");
+  const deleteSpan = document.createElement("span");
 
-    textSpan.textContent = value;
-    textSpan.classList.add('w-full');
+  listItem.classList.add("flex");
+  listItem.classList.add("flex-row");
 
-    listItem.classList.add("flex");
-    listItem.classList.add("flex-row");
+  textSpan.textContent = value;
+  textSpan.classList.add('w-full');
+  textSpan.classList.add('text-center');
 
-    listItem.appendChild(textSpan);
+  deleteSpan.textContent = "X";
+  deleteSpan.classList.add('w-[30%]');
+  deleteSpan.classList.add('text-blue-vert-fonce');
 
-    list.appendChild(listItem);
-    stuendts.push(value);
-    theList.push(value);
-    console.log(stuendts.length);
+  deleteSpan.addEventListener("click", handleDeleteName);
+
+  listItem.appendChild(textSpan);
+  listItem.appendChild(deleteSpan);
+  participantsList.appendChild(listItem);
+
+  names.push(value);
+  originalList.push(value);
+  inputText.value = "";
+}
+
+function handleDeleteName(event) {
+  let name = event.target.parentElement.textContent;
+  name = name.slice(0, -1);
+  const indexN = names.indexOf(name);
+  const indexO = originalList.indexOf(name);
+  const indexR = randomList.indexOf(name);
+
+  names.splice(indexN, 1);
+  originalList.splice(indexO, 1);
+  randomList.splice(indexR, 1);
+
+  const spans = document.querySelectorAll("span");
+  spans.forEach((span) => {
+    if (span.textContent === name) {
+      event.target.parentElement.remove();
+    }
+  });
+  
+  for (let i = 0; i < 15; i++) {
+    const chair = document.getElementById(`${i + 1}`);
+    chair.textContent = "";
+  }
+  if (participantsList1.innerHTML.trim() == "") {
+    participantsList1.classList.add("hidden");
+  }
+  else if (participantsList2.innerHTML.trim() == "") {
+    participantsList2.classList.add("hidden");
+  }
+  else if (participantsList1.innerHTML.trim() == "") {
+    participantsList3.classList.add("hidden");
+  }
 }
 
 function handleRandomButton() {
 
-  for(let i = 0; i < theList.length; i++) {
-    stuendts[i] = theList[i];
+  for(let i = 0; i < originalList.length; i++) {
+    names[i] = originalList[i];
   }
   
   while (true) {
-    const random = Math.floor(Math.random() * stuendts.length);
+    const random = Math.floor(Math.random() * names.length);
 
-    randomList.push(stuendts[random]);
+    randomList.push(names[random]);
 
-    stuendts.splice(random, 1);
+    names.splice(random, 1);
 
-    if (stuendts.length === 0) break;
+    if (names.length === 0) break;
   }
 
   for (let i = 0; i < randomList.length; i++) {
